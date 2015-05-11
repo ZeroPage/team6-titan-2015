7146c6495ad4860225e64f8ed7bbe3e2ba7810c2
Skywave <skywave@live.co.kr>
2015-05-11 23:10:55 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# team6-titan-2015
+Team project for 'software engineering(2015)'.
---
+++ src/Titan.java
@@ -0,0 +1,5 @@
+class Titan {
+    public static void main(String[] args) {
+        System.out.println("Hello, Titan!");
+    }
+}
