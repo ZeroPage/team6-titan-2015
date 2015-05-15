e7457d0ff7a65ddd4ee90fd2af746bf45b26f6b2
archetype <archetype93@gmail.com>
2015-05-15 11:54:12 +0900

    saveData method & more getMethod implemented
    deleted not using statements also.
    I have a small implementing issue : #3

--- src/model/ClusterData.java
+++ src/model/ClusterData.java
@@ -1,22 +1,26 @@
 package model;
 
 import java.util.*;
-import java.io.*;
-import java.lang.Exception.*;
+import java.io.File;
+import java.io.IOException;
 
 import javax.xml.parsers.*;
+import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.NamedNodeMap;
 import org.xml.sax.SAXException;
 
 public class ClusterData {
 	private Document doc;
 	private File sourceFile;
-	private String groupName;
 	private ArrayList<Element> groupList = new ArrayList<Element>();
 	
 	public ClusterData() {
@@ -62,7 +66,6 @@ public class ClusterData {
 			
 			if(node.getNodeType()==Node.ELEMENT_NODE) {
 				Element elem = (Element) node;
-				this.groupName = elem.getAttribute("name");
 				
 				NodeList groupNodeList = elem.getChildNodes();
 				for(int j=0;j<groupNodeList.getLength();j++) {
@@ -72,14 +75,19 @@ public class ClusterData {
 						Element gElem = (Element) groupNode;
 						this.groupList.add(gElem);
 					}
-					
 				}
 			}
 		}
 	}
 	
-	public void saveClusterData() {
-		//to be implemented
+	public void saveClusterData() throws TransformerException {
+		TransformerFactory tFac = TransformerFactory.newInstance();
+		Transformer transformer = tFac.newTransformer();
+		DOMSource source = new DOMSource(this.doc);
+		Result output = new StreamResult(this.sourceFile);
+		
+		this.sourceFile.delete();
+		transformer.transform(source, output);
 	}
 
 //give whole tree
@@ -89,9 +97,25 @@ public class ClusterData {
 
 //give group node with given name(if there's no node with the name, return null)
 	public Element getGroup(String groupName) {
-		for(Element group : groupList) {
+		for(int i=0;i<this.groupList.size();i++) {
+			Element group = groupList.get(i);
 			if(group.getAttribute("name").equals(groupName)) {
 				return group;
+			}
+		}
+		return null;
+	}
+
+//get specific item recognized with groupName and itemName
+	public Element getItem(String groupName, String itemName) {
+		Element groupLevel = getGroup(groupName);
+		NodeList inGroup = groupLevel.getChildNodes();
+		for(int i=0;i<inGroup.getLength();i++) {
+			Node item=inGroup.item(i);
+			if(item.getNodeType()==Node.ELEMENT_NODE) {
+				Element elemItem = (Element) item;
+				if(elemItem.getAttribute("name").equals(itemName))
+					return elemItem;
 			}
 		}
 		return null;
