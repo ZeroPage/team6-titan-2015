e00e19f99c2781b40c9ab29b02e2436f59ae60c8
archetype <archetype93@gmail.com>
2015-05-15 22:06:25 +0900

    Fatory class - transfroming btwn DOM and DMT
    Designed as a factory interface, in singleton pattern.
    TreeNode to Doc / Doc to TreeNode.
    Details will be implemented in few days.

---
+++ src/controller/ClusterTreeFactory.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class ClusterTreeFactory {
+	private static ClusterTreeFactory factoryInstance = new ClusterTreeFactory();
+	
+	private ClusterTreeFactory() {
+		//Overriding default constructor with private constructor
+	}
+	
+	public static ClusterTreeFactory getInstance() {
+		return factoryInstance;
+	}
+
+//RKTP's part from this line
+		//devoted to RKTP
+	}
+	
+//Yong-ho's part from this line	
+		//devoted to Yongho
+	}
+}
+
