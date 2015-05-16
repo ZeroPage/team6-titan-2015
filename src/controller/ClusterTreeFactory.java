45924c1ad810a86fe29d78e17fa5b626cbdcd81f
archetype <archetype93@gmail.com>
2015-05-16 23:24:58 +0900

    imported requiring interfaces
    interfaces required for traversing each term of tree.
    also, default return(null) statement added for evading errors.

--- src/controller/ClusterTreeFactory.java
+++ src/controller/ClusterTreeFactory.java
@@ -1,5 +1,15 @@
 package controller;
 
+import java.util.*;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class ClusterTreeFactory {
 	private static ClusterTreeFactory factoryInstance = new ClusterTreeFactory();
 	
@@ -14,11 +24,13 @@ public class ClusterTreeFactory {
 //RKTP's part from this line
 	public Document buildDocInstance() {
 		//devoted to RKTP
+		return null;
 	}
 	
 //Yong-ho's part from this line	
 	public DefaultMutableTreeNode buildNodeInstance() {
 		//devoted to Yongho
+		return null;
 	}
 	*/
 }
