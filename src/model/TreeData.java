4856004ec01a0d568f54705ac7e182f35d7b1a2c
archetype <archetype93@gmail.com>
2015-05-19 20:49:36 +0900

    TreeData basical implementation
    Devoted to Yong-ho, for now.
    #13

--- src/model/TreeData.java
+++ src/model/TreeData.java
@@ -1,10 +1,40 @@
 package model;
 
 import java.util.*;
+import java.io.*;
+import javax.swing.tree.*;
+
+import org.xml.sax.SAXException;
 
 public class TreeData {
 	private TitanDSM dsmData;
 	private ClusterData cluster;
+	private DefaultMutableTreeNode treeRoot;
 	
-
+	public TreeData(File dsmFile) throws IOException, SAXException {
+		dsmData = new TitanDSM(dsmFile);
+		cluster = null;
+		treeRoot = null;
+	}
+	
+	public void loadClusterData(File clsxFile) throws IOException, SAXException {
+		cluster = new ClusterData(clsxFile);
+		buildTree(cluster.getTree());
+	}
+	
+	private DefaultMutableTreeNode buildTree() {
+		DefaultMutableTreeNode node=null;//This is temporary implementation.
+		
+		//IMPLEMENT REQUIRED
+		
+		return node;
+	}
+	
+	private void buildTree(TreeNode clsxTree) {
+		
+		//IMPLEMENT REQUIRED
+		
+	}
+	
+	//get,set,save,load to be implemented. Plz get on work.
 }
