c49be008731fb5293ea57f126de88a6ecb790858
archetype <archetype93@gmail.com>
2015-05-19 20:51:39 +0900

    Deleted non-using exceptions
    as catching several exceptions ins ClusterData class, those exceptions
    are needed no more in TreeData class.

--- src/model/TreeData.java
+++ src/model/TreeData.java
@@ -4,20 +4,18 @@ import java.util.*;
 import java.io.*;
 import javax.swing.tree.*;
 
-import org.xml.sax.SAXException;
-
 public class TreeData {
 	private TitanDSM dsmData;
 	private ClusterData cluster;
 	private DefaultMutableTreeNode treeRoot;
 	
-	public TreeData(File dsmFile) throws IOException, SAXException {
+	public TreeData(File dsmFile) throws IOException {
 		dsmData = new TitanDSM(dsmFile);
 		cluster = null;
 		treeRoot = null;
 	}
 	
-	public void loadClusterData(File clsxFile) throws IOException, SAXException {
+	public void loadClusterData(File clsxFile) throws IOException {
 		cluster = new ClusterData(clsxFile);
 		buildTree(cluster.getTree());
 	}
