f898fe61e767ce486ab242ef843ed72525ca47e4
archetype <archetype93@gmail.com>
2015-05-27 23:56:25 +0900

    Total modification on TreeData
    1) treeRoot is no use
    2) building dsm with size is added
    3) getting dsm value is much more efficient(Group, item both available)
    4) Elem -> Element

--- src/model/TreeData.java
+++ src/model/TreeData.java
@@ -11,49 +11,91 @@ import javax.swing.tree.TreeNode;
 public class TreeData {
 	private TitanDSM dsmData;
 	private ClusterData cluster;
-	private TreeNode treeRoot;
 
 //initializing with only DSM
 	public TreeData(File dsmFile) throws IOException, WrongDSMFormatException {
 		dsmData = new TitanDSM(dsmFile);
-		cluster = null;
-		treeRoot = buildDefaultTree();
+		cluster = new ClusterData(this.dsmData);
+	}
+	
+	public TreeData(int size) throws IOException, WrongDSMFormatException, NotPositiveException {
+		dsmData = new TitanDSM(size);
+		cluster = new ClusterData(this.dsmData);
 	}
 
 //load clsx, and rebuild the data tree structure
 	public void loadClusterData(File clsxFile) throws IOException, WrongXMLNamespaceException {
 		cluster = new ClusterData(clsxFile);
 		cluster.refresh(this.dsmData);
-		treeRoot = cluster.getTree();
 	}
 	
-	public boolean getGroupDSM(DefaultMutableTreeNode group, DefaultMutableTreeNode elem) {
+	public boolean getDSMValue(DefaultMutableTreeNode rowElement, DefaultMutableTreeNode columnElement) {
 		boolean result = false;
-		if(!elem.getAllowsChildren()) {
-			for(int i=0;i<group.getChildCount();i++) {
-				DefaultMutableTreeNode groupItem = (DefaultMutableTreeNode)group.getChildAt(i);
-				String rowName = groupItem.getUserObject().toString();
-				String colName = elem.getUserObject().toString();
-				if(result = dsmData.getData(rowName,colName)) {
-					return result;
-				}
+		
+		if(rowElement.getAllowsChildren()) {
+			if(columnElement.getAllowsChildren()) {
+				result = getGroupGroupValue(rowElement,columnElement);
+			} else {
+				result = getGroupItemValue(rowElement, columnElement);
 			}
+			
 		} else {
-			for(int i=0;i<group.getChildCount();i++) {
-				DefaultMutableTreeNode groupItem = (DefaultMutableTreeNode)group.getChildAt(i);
-				String rowName = groupItem.getUserObject().toString();
-				for(int j=0;j<elem.getChildCount();j++) {
-					DefaultMutableTreeNode elemItem = (DefaultMutableTreeNode)elem.getChildAt(j);
-					String colName = elemItem.getUserObject().toString();
-					if(result = dsmData.getData(rowName, colName)) {
-						return result;
-					}
+			if(columnElement.getAllowsChildren()) {
+				result = getItemGroupValue(columnElement, rowElement);
+			} else {
+				String row = rowElement.getUserObject().toString();
+				String column = columnElement.getUserObject().toString();
+				result = dsmData.getData(row, column);
+			}
+		}
+
+		return result;
+	}
+	
+	private boolean getGroupGroupValue(DefaultMutableTreeNode rGroup, DefaultMutableTreeNode cGroup) {
+		boolean result = false;
+		
+		for(int i=0;i<rGroup.getChildCount();i++) {
+			DefaultMutableTreeNode rItem = (DefaultMutableTreeNode)rGroup.getChildAt(i);
+			String row = rItem.getUserObject().toString();
+			for(int j=0;j<cGroup.getChildCount();j++) {
+				DefaultMutableTreeNode cItem = (DefaultMutableTreeNode)cGroup.getChildAt(j);
+				String column = cItem.getUserObject().toString();
+				if(result = dsmData.getData(row, column)) {
+					return result;
 				}
 			}
 		}
 		return result;
 	}
 	
+	private boolean getGroupItemValue(DefaultMutableTreeNode group, DefaultMutableTreeNode element) {
+		boolean result = false;
+		
+		for(int i=0;i<group.getChildCount();i++) {
+			DefaultMutableTreeNode item = (DefaultMutableTreeNode)group.getChildAt(i);
+			String row = item.getUserObject().toString();
+			String column = element.getUserObject().toString();
+			if(result = dsmData.getData(row,column)) {
+				return result;
+			}
+		}
+		return result;
+	}
+	
+	private boolean getItemGroupValue(DefaultMutableTreeNode element, DefaultMutableTreeNode group) {
+		boolean result = false;
+		
+		for(int i=0;i<group.getChildCount();i++) {
+			DefaultMutableTreeNode item = (DefaultMutableTreeNode)group.getChildAt(i);
+			String row = element.getUserObject().toString();
+			String column = item.getUserObject().toString();
+			if(result = dsmData.getData(row,column)) {
+				return result;
+			}
+		}
+		return result;
+	}
 	
 	public void loadDSM(String dsmFileName) throws IOException, WrongDSMFormatException{
 		this.dsmData = new TitanDSM(new File(dsmFileName));
@@ -63,42 +105,36 @@ public class TreeData {
 			cluster.refresh(this.dsmData);
 		}
 	}
-	
-	public boolean getDSMvalue(DefaultMutableTreeNode row, DefaultMutableTreeNode col) {
-		String rowName = row.getUserObject().toString();
-		String colName = col.getUserObject().toString();
-		return dsmData.getData(rowName,colName);
-	}
 
 //rename the element(Group, Item both)
-	public void renameElem(DefaultMutableTreeNode currentNode, String newName) throws ItemAlreadyExistException, NodeNotFoundException {
+	public void renameElement(DefaultMutableTreeNode currentNode, String newName) throws ItemAlreadyExistException, NodeNotFoundException {
 		if(!currentNode.getAllowsChildren()) {
 			dsmData.setName(newName, currentNode.getUserObject().toString());
 		}
 		cluster.renameNode(currentNode, newName);
 	}
 	
-	public void repositionElem(DefaultMutableTreeNode elemNode,int newIndex) throws NodeNotFoundException {
-		cluster.moveNode(elemNode, newIndex);
+	public void repositionElement(DefaultMutableTreeNode elementNode,int newIndex) throws NodeNotFoundException {
+		cluster.moveNode(elementNode, newIndex);
 		//Does DSM has something to do with this method?
 	}
 	
-	public void removeElem(DefaultMutableTreeNode elemNode) throws NodeNotFoundException {
-		if(elemNode.getAllowsChildren()) {
+	public void removeElement(DefaultMutableTreeNode elementNode) throws NodeNotFoundException {
+		if(elementNode.getAllowsChildren()) {
 			//Case 1: the element was group - subtree has to be deleted.
 		} else {
 			//Case 2: the element was item - delete only stated element.
 		}
-		cluster.deleteItem(elemNode);
+		cluster.deleteItem(elementNode);
 	}
 	
-	public void addElem(DefaultMutableTreeNode groupNode, String itemName) throws NodeNotFoundException {
+	public void addElement(DefaultMutableTreeNode groupNode, String itemName) throws NodeNotFoundException {
 		cluster.addItem(groupNode, itemName);
 		//DSM team, Plz add your codes that are needed.
 	}
 	
-	public void groupElem(ArrayList<DefaultMutableTreeNode> elemList, String groupName) {
-		cluster.newGroupbyNode(elemList, groupName);
+	public void groupElement(ArrayList<DefaultMutableTreeNode> elementList, String groupName) {
+		cluster.newGroupbyNode(elementList, groupName);
 	}
 	
 	public void freeGroup(DefaultMutableTreeNode groupNode) throws NodeNotFoundException {
@@ -106,7 +142,7 @@ public class TreeData {
 	}
 	
 //build temporary cluster with DSM only.
-	private TreeNode buildDefaultTree() {
+	private DefaultMutableTreeNode buildDefaultTree() {
 		DefaultMutableTreeNode root = new DefaultMutableTreeNode("ROOT",true);
 		for(int i=0;i<this.dsmData.getSize();i++) {
 			root.add(new DefaultMutableTreeNode(this.dsmData.getName(i),false));
@@ -114,8 +150,8 @@ public class TreeData {
 		return root;
 	}
 	
-   public TreeNode getTree(){
-		return this.treeRoot;
+   public DefaultMutableTreeNode getTree(){
+		return this.cluster.getTree();
 	} 
 	
 	public void saveDSMData(File dsmFile) throws IOException{
