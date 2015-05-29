915240a005791fa4c06163fff53f2fbfeeb9cbec
Skywave <skywave@live.co.kr>
2015-05-29 17:08:16 +0900

    changed look and feel

--- src/Titan.java
+++ src/Titan.java
@@ -1,7 +1,22 @@
 import controller.TitanMainController;
 
+import javax.swing.*;
+
 class Titan {
     public static void main(String[] args) {
+
+        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            try {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            } catch (Exception e) {
+                // If it's not available, just use default L&F
+                break;
+            }
+        }
+
         new TitanMainController().openDialog();
     }
 }
