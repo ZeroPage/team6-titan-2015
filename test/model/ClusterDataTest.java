05189d5689158296ec941fe8d942d37f2476aecd
HeeJeong <cho2hol@cau.ac.kr>
2015-06-02 23:25:46 +0900

    add initial ClusterDataTest

---
+++ test/model/ClusterDataTest.java
@@ -0,0 +1,68 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClusterDataTest {
+
+    @Test
+    public void testLoadClusterData() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTree() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllowsChildren() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNode() throws Exception {
+
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+
+    }
+
+    @Test
+    public void testSaveClusterData() throws Exception {
+
+    }
+
+    @Test
+    public void testMoveNode() throws Exception {
+
+    }
+
+    @Test
+    public void testNewGroupbyNode() throws Exception {
+
+    }
+
+    @Test
+    public void testFreeGroup() throws Exception {
+
+    }
+
+    @Test
+    public void testRenameNode() throws Exception {
+
+    }
+
+    @Test
+    public void testAddItem() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteItem() throws Exception {
+
+    }
+}
