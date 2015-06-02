b09e384af810cd2e1cb61276f187b183c5bebcb3
HeeJeong <cho2hol@cau.ac.kr>
2015-06-02 23:01:32 +0900

    add initial ClusterFileIO test file

---
+++ test/model/ClusterFileIOTest.java
@@ -0,0 +1,23 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClusterFileIOTest {
+
+    @Test
+    public void testGetInstance() throws Exception {
+
+    }
+
+    @Test
+    public void testSaveClusterData() throws Exception {
+
+    }
+
+    @Test
+    public void testLoadClusterData() throws Exception {
+
+    }
+}
